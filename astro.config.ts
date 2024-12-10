6ab37e41049505bb1793dea62f991744cc211a31
Amber <wzhzzmzzy@gmail.com>
2024-12-10 22:08:58 +0800

    chore: init repo

---
+++ astro.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import UnoCss from 'unocss/astro';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare(),
+  integrations: [
+      UnoCss()
+  ]
+});
