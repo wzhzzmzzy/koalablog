b3875ab4e24835d38d1cbb590eeb1662d01dfe69
wzhzzmzzy <wzhzzmzzy@gmail.com>
2025-01-16 21:13:55 +0800

    fix: add noExternal

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'astro/config';
-import UnoCss from 'unocss/astro';
+import cloudflare from '@astrojs/cloudflare'
+import { defineConfig } from 'astro/config'
 
-import cloudflare from '@astrojs/cloudflare';
+import UnoCss from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   output: 'server',
   integrations: [
-      UnoCss()
-  ]
-});
+    UnoCss(),
+  ],
+  vite: {
+    ssr: {
+      noExternal: [
+        '@prisma/client',
+      ],
+    },
+  },
+})
+
