34983d7365cd3837e5fcd6f53b4089cb4c4041e8
wzhzzmzzy <wzhzzmzzy@gmail.com>
2025-04-09 11:15:38 +0800

    fix: use baseURL as client

--- src/lib/utils/auth-client.ts
+++ src/lib/utils/auth-client.ts
@@ -1,5 +1,3 @@
 import { createAuthClient } from 'better-auth/client'
 
-export const authClient = createAuthClient({
-  baseURL: 'http://localhost:4321/api/auth',
-})
+export const authClient = createAuthClient()
