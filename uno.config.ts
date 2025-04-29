553102a59065c4b3e8a72a5dc77fa9a1d3ee8cb1
wzhzzmzzy <wzhzzmzzy@gmail.com>
2025-04-29 21:17:48 +0800

    chore: add transformerDirectives

--- uno.config.ts
+++ uno.config.ts
@@ -1,16 +1,16 @@
 import presetIcons from '@unocss/preset-icons'
 import { presetRemToPx } from '@unocss/preset-rem-to-px'
-import { defineConfig, presetUno } from 'unocss'
+import { defineConfig, presetUno, transformerDirectives } from 'unocss'
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetIcons(),
-    presetRemToPx({
-      baseFontSize: 16,
-    }),
   ],
   safelist: [
     ...[true, false].map(show => `i-tabler:arrow-badge-${show ? 'left' : 'right'}-filled`),
+  ],
+  transformers: [
+    transformerDirectives(),
   ],
 })
