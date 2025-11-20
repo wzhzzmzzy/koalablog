de910f936bc3cf4440005b29d9e016e448c4daf4
wzhzzmzzy <wzhzzmzzy@gmail.com>
2025-11-20 14:12:42 +0800

    feat: add toml

--- src/lib/markdown/shiki.ts
+++ src/lib/markdown/shiki.ts
@@ -1,76 +1,95 @@
-import type MarkdownIt from 'markdown-it'
-import type { CatppuccinTheme } from '../const/config'
-import type { GlobalConfig } from '../kv'
-import { fromHighlighter } from '@shikijs/markdown-it/core'
-import { createHighlighterCore, createJavaScriptRegexEngine, type HighlighterGeneric } from 'shiki'
-import { wrapperlessFenceRule } from './wrapperless-fence-rule'
+import type MarkdownIt from "markdown-it";
+import type { CatppuccinTheme } from "../const/config";
+import type { GlobalConfig } from "../kv";
+import { fromHighlighter } from "@shikijs/markdown-it/core";
+import {
+  createHighlighterCore,
+  createJavaScriptRegexEngine,
+  type HighlighterGeneric,
+} from "shiki";
+import { wrapperlessFenceRule } from "./wrapperless-fence-rule";
 
 export interface HighlightOptions {
-  theme?: CatppuccinTheme
-  themeConfig?: ThemeConfig
-  langSet?: string[]
+  theme?: CatppuccinTheme;
+  themeConfig?: ThemeConfig;
+  langSet?: string[];
 }
 
-export type ThemeConfig = GlobalConfig['pageConfig']['theme']
+export type ThemeConfig = GlobalConfig["pageConfig"]["theme"];
 
-export async function useShiki(instance: MarkdownIt, options: HighlightOptions) {
-  const { theme: renderTheme, themeConfig, langSet } = options
+export async function useShiki(
+  instance: MarkdownIt,
+  options: HighlightOptions,
+) {
+  const { theme: renderTheme, themeConfig, langSet } = options;
 
-  let theme: string | null = null
-  let lightTheme: string | null = null
-  let darkTheme: string | null = null
+  let theme: string | null = null;
+  let lightTheme: string | null = null;
+  let darkTheme: string | null = null;
 
   if (!import.meta.env.SSR) {
-    const pageEl = globalThis.window?.document.querySelector('#page') as HTMLDivElement
-    const { lightTheme: l, darkTheme: d } = pageEl?.dataset || {}
-    const query = window.matchMedia('(prefers-color-scheme: dark)')
-    lightTheme = l || null
-    darkTheme = d || null
-    theme = (query.matches ? darkTheme : lightTheme) || 'latte'
-  }
-  else {
-    theme = renderTheme || themeConfig?.light || 'latte'
-    lightTheme = themeConfig?.light || null
-    darkTheme = themeConfig?.dark || null
+    const pageEl = globalThis.window?.document.querySelector(
+      "#page",
+    ) as HTMLDivElement;
+    const { lightTheme: l, darkTheme: d } = pageEl?.dataset || {};
+    const query = window.matchMedia("(prefers-color-scheme: dark)");
+    lightTheme = l || null;
+    darkTheme = d || null;
+    theme = (query.matches ? darkTheme : lightTheme) || "latte";
+  } else {
+    theme = renderTheme || themeConfig?.light || "latte";
+    lightTheme = themeConfig?.light || null;
+    darkTheme = themeConfig?.dark || null;
   }
 
   const has = (lang: string) =>
-    !import.meta.env.SSR ? langSet?.length ? lang.split(',').some(l => langSet.includes(l)) : true : true
+    !import.meta.env.SSR
+      ? langSet?.length
+        ? lang.split(",").some((l) => langSet.includes(l))
+        : true
+      : true;
 
   const highlighter = await createHighlighterCore({
     themes: [
-      [theme, lightTheme, darkTheme].includes('latte') && import('@shikijs/themes/catppuccin-latte'),
-      [theme, lightTheme, darkTheme].includes('frappe') && import('@shikijs/themes/catppuccin-frappe'),
-      [theme, lightTheme, darkTheme].includes('macchiato') && import('@shikijs/themes/catppuccin-macchiato'),
-      [theme, lightTheme, darkTheme].includes('mocha') && import('@shikijs/themes/catppuccin-mocha'),
-    ].filter(i => !!i),
+      [theme, lightTheme, darkTheme].includes("latte") &&
+        import("@shikijs/themes/catppuccin-latte"),
+      [theme, lightTheme, darkTheme].includes("frappe") &&
+        import("@shikijs/themes/catppuccin-frappe"),
+      [theme, lightTheme, darkTheme].includes("macchiato") &&
+        import("@shikijs/themes/catppuccin-macchiato"),
+      [theme, lightTheme, darkTheme].includes("mocha") &&
+        import("@shikijs/themes/catppuccin-mocha"),
+    ].filter((i) => !!i),
     langs: [
-      has('md,markdown') && import('@shikijs/langs/markdown'),
-      has('jsx') && import('@shikijs/langs/jsx'),
-      has('ts,typescript') && import('@shikijs/langs/typescript'),
-      has('js,javascript') && import('@shikijs/langs/javascript'),
-      has('rs,rust') && import('@shikijs/langs/rust'),
-      has('hs,haskell') && import('@shikijs/langs/haskell'),
-      has('py,python') && import('@shikijs/langs/python'),
-      has('json') && import('@shikijs/langs/json'),
-      has('ini') && import('@shikijs/langs/ini'),
-      has('kotlin') && import('@shikijs/langs/kotlin'),
-      has('yaml') && import('@shikijs/langs/yaml'),
-      has('shell') && import('@shikijs/langs/shell'),
-    ].filter(i => !!i),
+      has("md,markdown") && import("@shikijs/langs/markdown"),
+      has("jsx") && import("@shikijs/langs/jsx"),
+      has("ts,typescript") && import("@shikijs/langs/typescript"),
+      has("js,javascript") && import("@shikijs/langs/javascript"),
+      has("rs,rust") && import("@shikijs/langs/rust"),
+      has("hs,haskell") && import("@shikijs/langs/haskell"),
+      has("py,python") && import("@shikijs/langs/python"),
+      has("json") && import("@shikijs/langs/json"),
+      has("ini") && import("@shikijs/langs/ini"),
+      has("kotlin") && import("@shikijs/langs/kotlin"),
+      has("yaml") && import("@shikijs/langs/yaml"),
+      has("shell") && import("@shikijs/langs/shell"),
+      has("toml") && import("@shikijs/langs/toml"),
+    ].filter((i) => !!i),
     engine: createJavaScriptRegexEngine(),
-  })
+  });
 
-  instance.renderer.rules.fence = wrapperlessFenceRule
+  instance.renderer.rules.fence = wrapperlessFenceRule;
 
-  instance.use(fromHighlighter(highlighter as HighlighterGeneric<any, any>, {
-    theme: `catppuccin-${theme}`,
-    transformers: [
-      {
-        postprocess(html, { lang }) {
-          return `<div class="code-block"><span class="code-lang">${(lang || '').toUpperCase()}</span><div class="code-content">${html}</div></div>`
+  instance.use(
+    fromHighlighter(highlighter as HighlighterGeneric<any, any>, {
+      theme: `catppuccin-${theme}`,
+      transformers: [
+        {
+          postprocess(html, { lang }) {
+            return `<div class="code-block"><span class="code-lang">${(lang || "").toUpperCase()}</span><div class="code-content">${html}</div></div>`;
+          },
         },
-      },
-    ],
-  }))
+      ],
+    }),
+  );
 }
