35559de72288cbb61bde115f08d27c641b35f152
wzhzzmzzy <wzhzzmzzy@gmail.com>
2026-03-10 12:49:04 +0800

    Revert "fix: build error"
    This reverts commit 05557d83c1e7fcc431d75e85e48c0e0a1836f8f6.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -6,7 +6,7 @@ import { onboarding } from './form/onboarding'
 import { settings } from './form/settings'
 import { list, remove as removeResource, upload } from './oss/operate'
 
-export const server = {
+export const actions = {
   oss: {
     upload,
     remove: removeResource,
